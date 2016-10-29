34f010bca72b845acf30669cb435e857bfd6bfcd
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-10-29 22:00:00 +0200

    all

---
+++ PrzewodnikPakiet/R/maratony.R
@@ -0,0 +1,30 @@
+#' Marathons in Warsaw / Poland
+#'
+#' Data from Warsaw marathons 2013 - 2016. Based on https://www.orlenmarathon.pl/ database.
+#' The subsequent columns describe:
+#' \itemize{
+#' \item nazwisko_imie First and Last name
+#' \item nr.startowy Number
+#' \item plec Gender
+#' \item rok Year
+#' \item biegi_uliczne Name of the marathon
+#' \item czas_brutto Gross Time
+#' \item czas_brutto_sec Gross Time in secs
+#' \item czas_netto Net Time
+#' \item czas_netto_sec Net Time in sec
+#' \item grup_wiek Age group
+#' \item kategoria Category
+#' \item kraj_nazwa Country
+#' \item miejsce_kat Place in Category
+#' \item miejsce_open Place in Open
+#' }
+#'
+#' Prepared by Krzysztof Trajkowski.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name maratony
+#' @usage data(maratony)
+#' @format a data.frame with 70736 rows and 14 columns
+#'
+NULL
